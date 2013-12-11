701338bc08e7b96bf2f377cedc8a886db617c40a
Binyuk Dmytrij <dbinyuk@griddynamics.com>
2013-12-11 19:14:00 +0200

    test for cookbook modified according to new git repo

--- tests/svnhttp/test.sh
+++ tests/svnhttp/test.sh
@@ -2,22 +2,15 @@
 
 cd ~/vms/svnhttp-chef_solo
 vagrant reload --provision
-pushd /tmp
+cd /tmp
+[ -d myproject ] && rm -rf myproject
+mkdir myproject
 
-if test -d ./myproject/; then
-	rm -rf ./myproject/
-	echo "Directory removed: ./myproject/"
-fi
-mkdir ./myproject/
-
-echo "cloning repo"
-svn co --username=admin --password=admin http://127.0.0.1:8080/svn/myproject/ && echo ok
-
-
-echo Dima is clever > myproject/README.md
+echo "*** cloning repo"
+svn co --username=admin --password=admin http://127.0.0.1:8080/svn/myproject/
+echo "new line" > myproject/README.md
 cd myproject/
-svn status
 svn add README.md
-echo "committing"
+echo "*** committing"
 svn commit -m 'README added'
-echo SUCCESS
+echo "*** SUCCESS"
